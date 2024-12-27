2084bbd538e01e58bb282846aaa966123c40e031
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2024-12-27 10:55:00 +0100

    feat(links); import module into app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { HttpApiModule } from './http-api/http-api.module';
 import { LoggerModule } from 'src/contexts/shared/logger/infrastructure/logger.module';
 import { ConfigModule } from '@nestjs/config'
+import { LinkModule } from '@src/contexts/links/infrastructure/link.module';
 @Module({
-  imports: [HttpApiModule, LoggerModule, ConfigModule.forRoot({isGlobal: true, cache: true})],
+  imports: [HttpApiModule, LinkModule, LoggerModule, ConfigModule.forRoot({isGlobal: true, cache: true})],
   controllers: [],
   providers: [],
 })   
